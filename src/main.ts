81dce3d5fd3c71d612d7329c1996bbb3d298e33d
daviribeiro <luminuszz43@gmail.com>
2023-02-09 18:49:05 -0300

    remove http service

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { ConfigService } from '@nestjs/config';
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { HttpExceptionInterceptor } from '@infra/errors/http-exception.interceptor';
+
+import { ValidationPipe } from '@nestjs/common';
 
 (async () => {
-  const logger = new Logger('MainInstance');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'traking-service-client',
+          retry: {
+            retries: 2,
+          },
+
+          brokers: [process.env.KAFKA_CONECT_URL],
+        },
+
+        consumer: {
+          groupId: 'traking-service-consumer',
+        },
+      },
+    },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({ stopAtFirstError: true, transform: true }),
   );
-  app.useGlobalInterceptors(new HttpExceptionInterceptor());
 
-  const configService = app.get(ConfigService);
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.KAFKA,
-    options: {
-      client: {
-        clientId: 'traking-service-client',
-        retry: {
-          retries: 2,
-        },
-
-        brokers: [configService.get('KAFKA_CONECT_URL')],
-      },
-
-      consumer: {
-        groupId: 'traking-service-consumer',
-      },
-    },
-  });
-
-  app.startAllMicroservices().then(() => logger.log('Microservices started'));
-  app.listen(3003).then(() => logger.log('http Server started'));
+  await app.listen();
 })();
